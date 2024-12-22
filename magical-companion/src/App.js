dccd22c332e2f3d60978a360c6d57f66db633334
Pasindu Gamage <udnisap208@gmail.com>
2024-12-22 23:04:50 +1030

    initial commit

---
+++ magical-companion/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import HomePage from "./pages/home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+])
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
