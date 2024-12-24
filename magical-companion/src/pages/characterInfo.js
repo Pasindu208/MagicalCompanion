cdb65a4fe81609701c8bf6d22be3e6d83f6f6540
Pasindu Gamage <udnisap208@gmail.com>
2024-12-24 20:58:21 +1030

    Update characterInfo.js

--- magical-companion/src/pages/characterInfo.js
+++ magical-companion/src/pages/characterInfo.js
@@ -14,7 +14,7 @@ const CharacterInfo = () => {
         try {
             const response = await axios.get(
                 // `https://api.potterdb.com/v1/characters?filter[name_eq]=${name}`
-                `https://potterhead-api.vercel.app/api/characters/harry%20potter`
+                `https://cors-anywhere.herokuapp.com/https://potterhead-api.vercel.app/api/characters/harry%20potter`
             );
             // const response = await axios.get('https://api.potterdb.com/v1/characters');
             console.log(response.data);
